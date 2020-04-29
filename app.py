7a69fc1e2e554796ca25f2dd0ee982b2edcbab68
scotttheer <scotttheer@gmail.com>
2020-04-29 11:51:13 -0400

    Updating Flask app name

--- app.py
+++ app.py
@@ -3,27 +3,27 @@ import logging
 import os
 import time
 
-app = Flask(__name__)
-app.secret_key = 'r29J}K`Tc(w;Fk>*#u"7LR*OH#G7.t'
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+NVIapp = Flask(__name__)
+NVIapp.secret_key = 'r29J}K`Tc(w;Fk>*#u"7LR*OH#G7.t'
+NVIapp.config['UPLOAD_FOLDER'] = 'static/uploads'
 formats = ['xlsx', 'xls', 'xlsm', 'pdf', 'txt', 'doc']
 app_host_ip = '0.0.0.0'
 app_host_port = 8000
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-@app.route('/public')
+@NVIapp.route('/public')
 def index():
     return render_template('home.html')
 
-@app.route('/library')
+@NVIapp.route('/library')
 def library():
     return render_template('file.html')
 
-@app.route('/notes')
+@NVIapp.route('/notes')
 def notes():
     return render_template('notes.html')
 
-@app.route('/validate', methods=['POST'])
+@NVIapp.route('/validate', methods=['POST'])
 def validate():
     args = request.json
     print(args)
@@ -37,7 +37,7 @@ def main():
     console.setLevel(logging.DEBUG)
     logging.getLogger("werkzeug").addHandler(console)
 
-    app.run(host = app_host_ip, port= app_host_port, debug=True)
+    NVIapp.run(host = app_host_ip, port= app_host_port, debug=True)
 
 if __name__== '__main__':
     main()
